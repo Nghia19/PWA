e9ebf5affc100026d45099e230a34115eb42a537
Nghia19 <trungnghia191919@gmail.com>
2021-06-14 16:46:52 +0700

    add

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,18 @@
 const staticProducts = "PWA";
 const assets = [
   "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/img/product-1.jpg",
-  "/img/product-2.jpg",
-  "/img/product-3.jpg",
-  "/img/product-4.jpg",
-  "/img/product-5.jpg",
-  "/img/product-6.jpg",
-  "/img/product-7.jpg",
-  "/img/product-8.jpg",
-  "/img/product-9.jpg",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./img/product-1.jpg",
+  "./img/product-2.jpg",
+  "./img/product-3.jpg",
+  "./img/product-4.jpg",
+  "./img/product-5.jpg",
+  "./img/product-6.jpg",
+  "./img/product-7.jpg",
+  "./img/product-8.jpg",
+  "./img/product-9.jpg",
 ];
 
 self.addEventListener("install", (installEvent) => {
